241b182d29c2a1d9dd89df5ba9c9d5b80d6b1f29
Aidan <aidanwebster987@gmail.com>
2024-09-01 00:47:20 -0500

    End of section 16 (payment processing)

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51PtzzjK2lxXPmh3UGyC3Xd1JGBCVO8WJ1UF3W3xfHFUoyike9wPU8aSjgTuxDrjylkTnA3mzKmeJlPxZaYRKQuQk00Dr7A1Zto'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51PtzzjK2lxXPmh3UGyC3Xd1JGBCVO8WJ1UF3W3xfHFUoyike9wPU8aSjgTuxDrjylkTnA3mzKmeJlPxZaYRKQuQk00Dr7A1Zto'
 };
